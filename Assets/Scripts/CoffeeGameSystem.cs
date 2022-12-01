a6cad907395f4fa15635979b0daab296dad12e4e
pwerty <pwertyman@naver.com>
2022-12-01 22:52:20 +0900

    CoffeeGame, Korean Font Updated

--- Assets/Scripts/CoffeeGameSystem.cs
+++ Assets/Scripts/CoffeeGameSystem.cs
@@ -5,37 +5,29 @@ using UnityEngine.UI;
 
 public class CoffeeGameSystem : MonoBehaviour
 {
-    // Make 6 Color
-    // Red, Orange, Yellow, Green, Blue, Purple
-    [SerializeField]
-    public Color redColor = new Color();
-    public Color orangeColor = new Color();
-    public Color yellowColor = new Color();
-    public Color greenColor = new Color();
-    public Color blueColor = new Color();
-    public Color purpleColor = new Color();
-
+    // Using 6 colors : Red, Orange, Yellow, Green, Blue, Purple
     public GameObject[] patternArray = new GameObject[7];
-    public Color[] colorArray = new Color[7];
+    public Color[] colorArray = new Color[6];
     public Color[] BWArray = new Color[2];
     public Image[] sliderBackGroundArray = new Image[6];
-    public Color[] tempColorArray = new Color[7];
+    public Color[] tempColorArray = new Color[6];
+    public Slider[, ] slider_2DArray = new Slider[4, 4];
+    public int[,] destArray = new int[4, 2];
+
     int selector = 0;
     int answerSelector = 0;
-    public Slider _testSlider;
+    int contents = 0;
+    bool isGameCompleted = false;
+    int runnerStarter = 0;
+    int runnerDestination = 1;
+    
 
     public GameObject answerPatternTile;
+    public Color playerColorSelection;
 
     // Start is called before the first frame update
     void Start()
     {
-        redColor = Color.red;
-        orangeColor = new Color(1, 0.5f, 0, 1);
-        yellowColor = Color.yellow;
-        greenColor = Color.green;
-        blueColor = Color.blue;
-        purpleColor = Color.magenta;
-
         BWArray[0] = Color.black;
         BWArray[1] = Color.white;
 
@@ -47,8 +39,9 @@ public class CoffeeGameSystem : MonoBehaviour
         colorArray[4] = Color.blue;
         colorArray[5] = Color.magenta;
 
-        patternGenerate();
-       // sliderColorGenarate();
+       SliderInitialize();
+       patternGenerate();
+       sliderColorGenarate();
     }
 
     // 플레이어가 수행해야 할 타일 패턴 7개를 정의하는 메소드이다
@@ -65,68 +58,134 @@ public class CoffeeGameSystem : MonoBehaviour
     // 윷놀이 보드 모양으로 배치된 슬라이더의 색을 배치하는 메소드이다
     public void sliderColorGenarate()
     {
-        for(int i = 0; i <= 6; i++)
-        {
-            tempColorArray[i] = colorArray[i];
-        }
+        // colorArray를 복사한다
+        // tempColorArray에 중복이 발생하지 않도록 준비하는 과정 중 하나이다
+        tempColorArray = (Color[])colorArray.Clone();
 
+        // tempColorArray의 색을 하나 뽑아서 사용하는데 black으로 이미 사용 중임을 표기한다
+        // 모든 색이 소모 될 때 까지 반복하도록 한다
         while(true)
         {
             bool isComplete = false;
 
-            for(int i=0; i<7; i++)
+            for(int i=0; i<6; i++)
             {
                 isComplete = true;
                 if(tempColorArray[i] != Color.black)
                 {
                     isComplete = false;
+                    break;
                 }
             }
 
             if (isComplete)
             {
                 selector = 0;
-                break;
+                return;
             }
 
             int randomIndex = Random.Range(0, 6);
 
             if(tempColorArray[randomIndex] != Color.black)
             {
-                Color targetColor = tempColorArray[randomIndex];
+                sliderBackGroundArray[selector].color = tempColorArray[randomIndex];
                 tempColorArray[randomIndex] = Color.black;
-                sliderBackGroundArray[selector].color = targetColor;
                 selector++;
             }
             else
             {
                 continue;
             }
+        }
+    }
 
-
+    public void SliderInitialize()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            for(int j = 0; j < 2; j++)
+            {
+                destArray[i,j] = -1;
+            }
         }
 
+        for(int i = 0; i < 4; i++)
+        {
+            for(int j = 0; j < 4; j++)
+            {
+                slider_2DArray[i, j] = null;
+            }
+        }
 
-        // get new color array copy from new array
-        // random apply to slider fill
-            // apply one color, and blank color in array
-        // if detect blank color, re-random
-        // if new color array dosen't have any black, can go
+        destArray[0, 0] = 1;
+        destArray[1, 0] = 2;
+        destArray[1, 1] = 3;
+        destArray[2, 0] = 0;
+        destArray[2, 1] = 3;
+        destArray[3, 0] = 0;
+
+        slider_2DArray[0, 1] = GameObject.Find("Slider0").GetComponent<Slider>();
+        slider_2DArray[1, 2] = GameObject.Find("Slider2").GetComponent<Slider>();
+        slider_2DArray[1, 3] = GameObject.Find("Slider1").GetComponent<Slider>();
+        slider_2DArray[2, 0] = GameObject.Find("Slider3").GetComponent<Slider>();
+        slider_2DArray[2, 3] = GameObject.Find("Slider4").GetComponent<Slider>();
+        slider_2DArray[3, 0] = GameObject.Find("Slider5").GetComponent<Slider>();
+        Debug.Log("Initiaize Completed!");
     }
 
     void Update()
-    {
-        _testSlider.value += 0.001f;
-        if (_testSlider.value >= 0.99f)
-        {
-            _testSlider.value = 0f;
-            answerPatternTile.GetComponent<RawImage>().color = colorArray[Random.Range(0, 6)];
-        }
-
-        // space를 누르면 채점을 시작한다
+    { 
+        // space를 누르면 채점을 시작한다.
         if(Input.GetKeyDown(KeyCode.Space))
         {
             scoring();
+        }
+
+        if(!isGameCompleted)
+        {
+            sliderRunner();
+        }
+
+    }
+
+
+
+
+    public void sliderRunner()
+    {
+        if (isGameCompleted) return;
+
+
+        Slider targetSlider = slider_2DArray[runnerStarter, runnerDestination];
+        answerPatternTile.GetComponent<RawImage>().color = targetSlider.gameObject.transform.Find("Fill Area/Fill").GetComponent<Image>().color;
+        
+        if (targetSlider.value < 1f)
+        {
+            targetSlider.value += 0.0009f;
+            
+        }
+        else
+        {
+
+            targetSlider.value = 0f;
+            Debug.Log(targetSlider.name + "의 작동을 마쳤습니다 ");
+            runnerStarter = runnerDestination;
+            runnerDestination = Shake(runnerStarter);
+            Debug.Log(slider_2DArray[runnerStarter, runnerDestination].name + "의 작동 시작 ");
+            sliderRunner();
+        }
+    }
+
+    public int Shake(int start)
+    {
+        while (true)
+        {
+            int r = Random.Range(0, 2);
+            if (destArray[start, r] != -1)
+            {
+                int contents = destArray[start, r];
+                return contents;
+            }
         }
     }
 
@@ -146,12 +205,16 @@ public class CoffeeGameSystem : MonoBehaviour
         else
         {
             Debug.Log("Wrong Answer");
+            //틀렸을 때의 누적 카운트도 셀 수 있다
+            //게임 개발 방향에 따라 수정 될 여지는 있다
         }
 
-        if (answerSelector >= 7) Debug.Log("Good!! Reward Here!!");
+        // 모든 타일을 맞춘 상황이다
+        if (answerSelector >= 7)
+        {
+            isGameCompleted = true;
+            Debug.Log("Good!! Reward Here!!");
 
-        //틀렸을 때의 누적 카운트도 셀 수 있도록 만들 생각이다
-
-        
+        }
     }
 }
