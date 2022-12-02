d0d2814546b8ad3d91d649a5a2f0af8f06c2681d
Sung Jun Cho <chosung0531@naver.com>
2022-12-03 03:41:02 +0900

    tutorial text
    tutorial text = my scene

---
+++ Assets/Tutorial_Text.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial_Text : MonoBehaviour
+{
+    private bool firtstText=true;
+    private bool secondText=false;
+    private bool thirdText=false;
+
+    public GameObject Text1;
+    public GameObject Text2;
+    public GameObject Text3;
+
+    private void Awake()
+    {
+        Text2.SetActive(false);
+        Text3.SetActive(false);
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (thirdText)
+            {
+                thirdText = false;
+                Text3.SetActive(false);
+             
+            }
+            if (secondText)
+            {
+                secondText = false;
+                thirdText = true;
+                Text2.SetActive(false);
+                Text3.SetActive(true);
+            }
+            if (firtstText)
+            {
+                firtstText = false;
+                secondText = true;
+                Text1.SetActive(false);
+                Text2.SetActive(true);
+            }
+            
+        }
+    }
+}
