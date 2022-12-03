65cad375bef6cf74c94e3a09efa292b7ac3df289
pwerty <pwertyman@naver.com>
2022-12-03 22:54:44 +0900

    timinggame text update
    아 춥다

--- Assets/Scripts/TimingGameSystem.cs
+++ Assets/Scripts/TimingGameSystem.cs
@@ -76,6 +76,12 @@ public class TimingGameSystem : MonoBehaviour
                 howMuchTimeSpended++;
                 scoreText.text = howMuchTimeSpended.ToString();
             }
+            else
+            {
+                howMuchTimeSpended = 0;
+            }
+
+            //make red-text animation when scores going 0
         }
 
 
